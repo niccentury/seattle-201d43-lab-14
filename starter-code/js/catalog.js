2bbb969d104cb7263d1c1c70f651b3740913e05e
Danul De Leon <danul.deleon@gmail.com>
2018-08-16 13:44:06 -0700

    populated dropdown menu

--- starter-code/js/catalog.js
+++ starter-code/js/catalog.js
@@ -11,8 +11,10 @@ function populateForm() {
 
   //TODO: Add an <option> tag inside the form's select for each product
   var selectElement = document.getElementById('items');
-  for (var i of Product.allProducts) {
-
+  for (var i = 0; i< Product.allProducts.length; i++) {
+    var option = document.createElement('option');
+    option.innerHTML = Product.allProducts[i].name;
+    selectElement.appendChild(option);
   }
 
 }
